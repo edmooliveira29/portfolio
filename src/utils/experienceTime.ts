074df56bcf57ba3b8c4bf0167bdd45b67c9da261
edmooliveira29 <edmooliveira29@gmail.com>
2024-08-20 19:53:55 -0300

    feat: create util to experience time

---
+++ src/utils/experienceTime.ts
@@ -0,0 +1,17 @@
+export const experienceTime = (date: string) => {
+  const dataAtual = new Date();
+  const data = new Date(date);
+
+  let anos = dataAtual.getFullYear() - data.getFullYear();
+  let meses = dataAtual.getMonth() - data.getMonth();
+
+  if (meses < 0) {
+    anos--;
+    meses += 12;
+  }
+  if (anos === 0) {
+    return `${meses} meses`;
+  } else {
+    return `${anos} anos e ${meses} meses`;
+  }
+}
