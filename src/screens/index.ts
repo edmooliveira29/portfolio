032c8f6e18b8cca9e531b82ec0d9eb29a345b2ab
edmooliveira29 <edmooliveira29@gmail.com>
2024-08-20 20:00:47 -0300

    fix: delete functions and files not used

--- src/screens/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './Home/Home'
-export * from './About/About'
-export * from './Projects/Projects'
-export * from './Contact/Contact'
--- src/theme/StyleSheet.ts
+++
@@ -1,9 +0,0 @@
-import { Breakpoints } from '@displaykit/responsive_styles';
-
-type ResponsiveProperty<Type> = Partial<Record<Breakpoints, Type>>;
-
-export interface StyleSheet {
-  fontFamily?: ResponsiveProperty<string> | string;
-  backgroundColor?: ResponsiveProperty<string> | string;
-  [key: string]: any;
-}
