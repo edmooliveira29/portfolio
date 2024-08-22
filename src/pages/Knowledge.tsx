febca0d86485be9e5d4fdc8b68a59a1fe4dcbc01
edmooliveira29 <edmooliveira29@gmail.com>
2024-08-21 23:57:58 -0300

    feat: create Home and Knowledge

---
+++ src/pages/Knowledge.tsx
@@ -0,0 +1,182 @@
+import React from 'react'
+
+
+const Knowledge = () => {
+  return (
+    <section className='sm:px-0 pt-5 py-5 text-white'>
+      <h1 className="mx-5 text-3xl text-white font-bold after:block after:w-8 after:h-1 after:bg-white after:mt-1">
+        CONHECIMENTOS
+      </h1>
+      <div className="lg:flex lg:justify-center items-center my-5">
+
+        <div className='w-full lg:w-1/2 px-5 lg:pr-10 flex-col content-between'>
+
+          <div className="mt-5 bg-gray-600 rounded-lg p-5 hover:scale-105 duration-500 hover:scale-105 duration-500">
+            <span className="text-xl font-bold">FRONTEND</span>
+            <span className="block text-md font-bold">Html</span>
+            <div className="flex w-full h-4 bg-gray-200 rounded-full overflow-hidden mb-1"
+              role="progressbar" aria-valuenow={95} aria-valuemin={0} aria-valuemax={100}>
+              <div className="flex flex-col justify-center rounded-full overflow-hidden bg-black/50 text-xs text-black text-center whitespace-nowrap transition duration-500" style={{ width: '95%' }}>95%</div>
+            </div>
+
+            <span className="block text-md font-bold">Css</span>
+            <div className="flex w-full h-4 bg-gray-200 rounded-full overflow-hidden mb-1"
+              role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100}>
+              <div className="flex flex-col justify-center rounded-full overflow-hidden bg-black/50 text-xs text-black text-center whitespace-nowrap transition duration-500" style={{ width: '90%' }}>90%</div>
+            </div>
+
+            <span className="block text-md font-bold">AngularJS </span>
+            <div className="flex w-full h-4 bg-gray-200 rounded-full overflow-hidden mb-1"
+              role="progressbar" aria-valuenow={80} aria-valuemin={0} aria-valuemax={100}>
+              <div className="flex flex-col justify-center rounded-full overflow-hidden bg-black/50 text-xs text-black text-center whitespace-nowrap transition duration-500" style={{ width: '95%' }}>80%</div>
+            </div>
+
+            <span className="block text-md font-bold">React</span>
+            <div className="flex w-full h-4 bg-gray-200 rounded-full overflow-hidden mb-1"
+              role="progressbar" aria-valuenow={95} aria-valuemin={0} aria-valuemax={100}>
+              <div className="flex flex-col justify-center rounded-full overflow-hidden bg-black/50 text-xs text-black text-center whitespace-nowrap transition duration-500" style={{ width: '95%' }}>95%</div>
+            </div>
+
+            <span className="block text-md font-bold">Tailwind</span>
+            <div className="flex w-full h-4 bg-gray-200 rounded-full overflow-hidden mb-1"
+              role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100}>
+              <div className="flex flex-col justify-center rounded-full overflow-hidden bg-black/50 text-xs text-black text-center whitespace-nowrap transition duration-500" style={{ width: '90%' }}>90%</div>
+            </div>
+            <span className="block text-md font-bold">NextJs</span>
+            <div className="flex w-full h-4 bg-gray-200 rounded-full overflow-hidden mb-1"
+              role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100}>
+              <div className="flex flex-col justify-center rounded-full overflow-hidden bg-black/50 text-xs text-black text-center whitespace-nowrap transition duration-500" style={{ width: '90%' }}>90%</div>
+            </div>
+            <span className="block text-md font-bold">Styled Components</span>
+            <div className="flex w-full h-4 bg-gray-200 rounded-full overflow-hidden mb-1"
+              role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100}>
+              <div className="flex flex-col justify-center rounded-full overflow-hidden bg-black/50 text-xs text-black text-center whitespace-nowrap transition duration-500" style={{ width: '85%' }}>85%</div>
+            </div>
+          </div>
+          <div className="mt-5 bg-gray-600 rounded-lg p-5 hover:scale-105 duration-500">
+            <span className="text-xl font-bold ">TESTES</span>
+            <span className="block text-md font-bold">Jest</span>
+            <div className="flex w-full h-4 bg-gray-200 rounded-full overflow-hidden mb-1"
+              role="progressbar" aria-valuenow={85} aria-valuemin={0} aria-valuemax={100}>
+              <div className="flex flex-col justify-center rounded-full overflow-hidden bg-black/50 text-xs text-black text-center whitespace-nowrap transition duration-500" style={{ width: '85%' }}>85%</div>
+            </div>
+            <span className="block text-md font-bold">Mocha</span>
+
+            <div className="flex w-full h-4 bg-gray-200 rounded-full overflow-hidden mb-1"
+              role="progressbar" aria-valuenow={85} aria-valuemin={0} aria-valuemax={100}>
+              <div className="flex flex-col justify-center rounded-full overflow-hidden bg-black/50 text-xs text-black text-center whitespace-nowrap transition duration-500" style={{ width: '85%' }}>85%</div>
+            </div>
+
+            <span className="block text-md font-bold">Karma</span>
+            <div className="flex w-full h-4 bg-gray-200 rounded-full overflow-hidden mb-1"
+              role="progressbar" aria-valuenow={70} aria-valuemin={0} aria-valuemax={100}>
+              <div className="flex flex-col justify-center rounded-full overflow-hidden bg-black/50 text-xs text-black text-center whitespace-nowrap transition duration-500" style={{ width: '70%' }}>70%</div>
+            </div>
+
+            <span className="block text-md font-bold">Cypress</span>
+            <div className="flex w-full h-4 bg-gray-200 rounded-full overflow-hidden mb-1"
+              role="progressbar" aria-valuenow={85} aria-valuemin={0} aria-valuemax={100}>
+              <div className="flex flex-col justify-center rounded-full overflow-hidden bg-black/50 text-xs text-black text-center whitespace-nowrap transition duration-500" style={{ width: '80%' }}>80%</div>
+            </div>
+
+            <span className="block text-md font-bold">Cucumber</span>
+            <div className="flex w-full h-4 bg-gray-200 rounded-full overflow-hidden mb-1"
+              role="progressbar" aria-valuenow={85} aria-valuemin={0} aria-valuemax={100}>
+              <div className="flex flex-col justify-center rounded-full overflow-hidden bg-black/50 text-xs text-black text-center whitespace-nowrap transition duration-500" style={{ width: '80%' }}>80%</div>
+            </div>
+            <span className="block text-md font-bold">Selenium</span>
+            <div className="flex w-full h-4 bg-gray-200 rounded-full overflow-hidden mb-1"
+              role="progressbar" aria-valuenow={85} aria-valuemin={0} aria-valuemax={100}>
+              <div className="flex flex-col justify-center rounded-full overflow-hidden bg-black/50 text-xs text-black text-center whitespace-nowrap transition duration-500" style={{ width: '80%' }}>80%</div>
+            </div>
+            
+          </div>
+
+          <div className="mt-5 bg-gray-600 rounded-lg p-5 hover:scale-105 duration-500">
+            <span className="text-xl font-bold">MOBILE</span>
+            <span className="block text-md font-bold">React Native</span>
+            <div className="flex w-full h-4 bg-gray-200 rounded-full overflow-hidden mb-1"
+              role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100}>
+              <div className="flex flex-col justify-center rounded-full overflow-hidden bg-black/50 text-xs text-black text-center whitespace-nowrap transition duration-500" style={{ width: '90%' }}>90%</div>
+            </div>
+          </div>
+        </div>
+
+        <div className='w-full lg:w-1/2 px-5 lg:pl-10'>
+          <div className="mt-5 lg:my-7 bg-gray-600 rounded-lg p-5 hover:scale-105 duration-500">
+            <span className="text-xl font-bold">BACKEND</span>
+            <span className="block text-md font-bold">NodeJs</span>
+            <div className="flex w-full h-4 bg-gray-200 rounded-full overflow-hidden mb-1"
+              role="progressbar" aria-valuenow={65} aria-valuemin={0} aria-valuemax={100}>
+              <div className="flex flex-col justify-center rounded-full overflow-hidden bg-black/50 text-xs text-black text-center whitespace-nowrap transition duration-500" style={{ width: '65%' }}>65%</div>
+            </div>
+
+            <span className="block text-md font-bold">MongoDB</span>
+            <div className="flex w-full h-4 bg-gray-200 rounded-full overflow-hidden mb-1"
+              role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100}>
+              <div className="flex flex-col justify-center rounded-full overflow-hidden bg-black/50 text-xs text-black text-center whitespace-nowrap transition duration-500" style={{ width: '60%' }}>60%</div>
+            </div>
+          </div>
+          <div className="mt-5 lg:my-7 bg-gray-600 rounded-lg p-5 hover:scale-105 duration-500">
+            <span className="text-xl font-bold">NO CODE</span>
+            <span className="block text-md font-bold">Wordpress</span>
+            <div className="flex w-full h-4 bg-gray-200 rounded-full overflow-hidden mb-1"
+              role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100}>
+              <div className="flex flex-col justify-center rounded-full overflow-hidden bg-black/50 text-xs text-black text-center whitespace-nowrap transition duration-500" style={{ width: '75%' }}>75%</div>
+            </div>
+
+            <span className="block text-md font-bold">Nuvemshop</span>
+            <div className="flex w-full h-4 bg-gray-200 rounded-full overflow-hidden mb-1"
+              role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100}>
+              <div className="flex flex-col justify-center rounded-full overflow-hidden bg-black/50 text-xs text-black text-center whitespace-nowrap transition duration-500" style={{ width: '90%' }}>90%</div>
+            </div>
+          </div>
+          <div className="mt-5 lg:my-7 bg-gray-600 rounded-lg p-5 hover:scale-105 duration-500">
+            <span className="text-xl font-bold">INFRAESTRUTURA</span>
+            <span className="block text-md font-bold">Google Cloud Compute Engine</span>
+            <div className="flex w-full h-4 bg-gray-200 rounded-full overflow-hidden mb-1"
+              role="progressbar" aria-valuenow={85} aria-valuemin={0} aria-valuemax={100}>
+              <div className="flex flex-col justify-center rounded-full overflow-hidden bg-black/50 text-xs text-black text-center whitespace-nowrap transition duration-500" style={{ width: '85%' }}>85%</div>
+            </div>
+
+            <span className="block text-md font-bold">Netlify</span>
+            <div className="flex w-full h-4 bg-gray-200 rounded-full overflow-hidden mb-1"
+              role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100}>
+              <div className="flex flex-col justify-center rounded-full overflow-hidden bg-black/50 text-xs text-black text-center whitespace-nowrap transition duration-500" style={{ width: '90%' }}>90%</div>
+            </div>
+            <span className="block text-md font-bold">Github CI e GitLab CI</span>
+            <div className="flex w-full h-4 bg-gray-200 rounded-full overflow-hidden mb-1"
+              role="progressbar" aria-valuenow={75} aria-valuemin={0} aria-valuemax={100}>
+              <div className="flex flex-col justify-center rounded-full overflow-hidden bg-black/50 text-xs text-black text-center whitespace-nowrap transition duration-500" style={{ width: '75%' }}>75%</div>
+            </div>
+          </div>
+          <div className="mt-5 lg:my-7 bg-gray-600 rounded-lg p-5 hover:scale-105 duration-500">
+            <span className="text-xl font-bold">Linguagens</span>
+            <span className="block text-md font-bold">Javascript</span>
+            <div className="flex w-full h-4 bg-gray-200 rounded-full overflow-hidden mb-1"
+              role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100}>
+              <div className="flex flex-col justify-center rounded-full overflow-hidden bg-black/50 text-xs text-black text-center whitespace-nowrap transition duration-500" style={{ width: '95%' }}>95%</div>
+            </div>
+            <span className="block text-md font-bold">Typescript</span>
+            <div className="flex w-full h-4 bg-gray-200 rounded-full overflow-hidden mb-1"
+              role="progressbar" aria-valuenow={90} aria-valuemin={0} aria-valuemax={100}>
+              <div className="flex flex-col justify-center rounded-full overflow-hidden bg-black/50 text-xs text-black text-center whitespace-nowrap transition duration-500" style={{ width: '90%' }}>88%</div>
+            </div>
+            <span className="block text-md font-bold">Python</span>
+            <div className="flex w-full h-4 bg-gray-200 rounded-full overflow-hidden mb-1"
+              role="progressbar" aria-valuenow={45} aria-valuemin={0} aria-valuemax={100}>
+              <div className="flex flex-col justify-center rounded-full overflow-hidden bg-black/50 text-xs text-black text-center whitespace-nowrap transition duration-500" style={{ width: '45%' }}>45%</div>
+            </div>
+          </div>
+
+        </div>
+
+
+      </div>
+
+
+    </section >
+  )
+}
+
+export default Knowledge
